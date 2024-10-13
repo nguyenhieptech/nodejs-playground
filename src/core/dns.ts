8582a8aedb9c5ce30570210fab3a24e1fff96b46
Hiep Nguyen <nguyenhieptech@gmail.com>
2024-10-13 21:21:33 +0700

    feat: dns module

---
+++ src/core/dns.ts
@@ -0,0 +1,18 @@
+// https://nodejs.org/docs/latest/api/dns.html#dns
+
+import dns from "node:dns";
+
+dns.resolve4("archive.org", (err, addresses) => {
+  if (err) throw err;
+
+  console.log(`addresses: ${JSON.stringify(addresses)}`);
+
+  addresses.forEach((a) => {
+    dns.reverse(a, (err, hostnames) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`reverse for ${a}: ${JSON.stringify(hostnames)}`);
+    });
+  });
+});
